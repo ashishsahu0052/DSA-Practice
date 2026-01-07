8485763ec8f6eef6dd818e050e42c377b75ea169
Ashish Sahu <ashishsahu0052@gmail.com>
2026-01-07 22:43:52 +0530

    practice

---
+++ l7.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int reverse(int x) {
+        String xStr = String.valueOf(x);
+        char[] xChar = xStr.toCharArray();
+
+        int start = xChar[0] == '-' ? 1 : 0;
+        int last = xChar.length - 1;
+
+        while(last > start) {
+            char temp = xChar[start];
+            xChar[start] = xChar[last];
+            xChar[last] = temp;
+            last--;
+            start++;
+        }
+
+         try {
+            return Integer.parseInt(new String(xChar));
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
