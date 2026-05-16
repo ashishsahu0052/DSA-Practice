045599196a3996eb001ce9bacd7b38688fb725bb
Ashish Sahu <ashishsahu0052@gmail.com>
2026-05-16 23:48:25 +0530

    practice

---
+++ dsa_practice/gfg39.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findSmallest(int[] arr) {
+        
+        Arrays.sort(arr);
+        
+        long res = 1;
+        
+        for(int num : arr){
+            
+            if(num > res){
+                break;
+            }
+            
+            res += num;
+        }
+        
+        return (int)res;
+    }
+}
